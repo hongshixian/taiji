fa5e890a4656df6e65a63675ea2a2197576f60c9
lihao <442401597@qq.com>
2026-08-20 21:04:48 +0800

    fix(auth): use combined username password login

--- scripts/iam-bootstrap-admin.sh
+++ scripts/iam-bootstrap-admin.sh
@@ -56,6 +56,7 @@ if [[ "${IAM_REALM_RECONCILE_ENABLED:-true}" == "true" ]]; then
   "${kcadm}" update "realms/${realm}" \
     -s loginTheme=taiji \
     -s accountTheme=fangcun-account \
+    -s organizationsEnabled=false \
     -s internationalizationEnabled=true \
     -s 'supportedLocales=["zh-CN","en"]' \
     -s defaultLocale=zh-CN >/dev/null
